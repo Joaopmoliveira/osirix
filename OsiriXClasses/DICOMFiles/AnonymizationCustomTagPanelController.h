eccaf3c1ff9e4a5a173dfe9b6e22b60099842ad3
= <joaomoliveira1996@gmail.com>
2020-12-14 18:39:59 +0000

    Anonymization Custom tag panel control documentation

--- OsiriXClasses/DICOMFiles/AnonymizationCustomTagPanelController.h
+++ OsiriXClasses/DICOMFiles/AnonymizationCustomTagPanelController.h
@@ -16,14 +16,29 @@
 
 @class DCMAttributeTag;
 
+/**
+ * Class used to interface with the @"AnonymizationCustomTagPanel" nib file. 
+ * It serves the porpouse of changing a given tag through the DCMAttributeTag class.
+ */
+
 @interface AnonymizationCustomTagPanelController : NSWindowController {
 	IBOutlet NSTextField* groupField;
 	IBOutlet NSTextField* elementField;
 }
 
+/**
+ * Reaction method to when the user selects the cancel button
+ */
 -(IBAction)cancelButtonAction:(id)sender;
+
+/**
+* Reaction method to when the user selects the ok button
+ */
 -(IBAction)okButtonAction:(id)sender;
 
+/**
+* Class used to store the tag, i.e. Dicom information and the relevant keys
+ */
 @property(assign) DCMAttributeTag* attributeTag;
 
 @end
